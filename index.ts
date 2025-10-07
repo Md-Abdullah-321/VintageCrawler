fea35dadf9bb8ddf3918a7f94aac6853996b5784
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2025-10-07 20:06:25 +0600

    fix: update jobs.json to include detailed job logs and completion status

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 import app from "./src/app.js";
 dotenv.config();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; 
 app.listen(port, () => {
   console.info(`Listening on http://localhost:${port}`);
 });
